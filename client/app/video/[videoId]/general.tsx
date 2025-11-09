dbe2f5a81e1b47e6818049ab71ac6ee3d18c3287
wp <97200483+weijiePan@users.noreply.github.com>
2025-11-08 19:44:55 -0500

    coordinate overlays

--- client/app/video/[videoId]/general.tsx
+++ client/app/video/[videoId]/general.tsx
@@ -1,11 +1,8 @@
 'use client'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import { start } from 'repl'
 
-function drawPoint(coordinateRect, videoRect) {
-  if (coordinateRect) {
-    console.log(coordinateRect)
-  }
-}
+
 
 export default function VideoBody({
   videoMetadata,
@@ -13,14 +10,44 @@ export default function VideoBody({
 videoMetadata: any
 }) {
   const perfectPathCoords = videoMetadata.ball_bboxes
-  const videoElementRef = useRef(null);
+  const videoElementRef = useRef<HTMLVideoElement>(null);
+  let canva = useRef<HTMLCanvasElement>(null);
+  console.log(videoMetadata);
+  useEffect(()=>{
+    console.log(videoMetadata);
+    const startFrame = videoMetadata.start_frame;
+    const endFrame = videoMetadata.end_frame;
+    
+    if(canva){
+      canva.current.height = videoElementRef.current?.height ;
+      canva.current.width = videoElementRef.current?.width;
+      const ballStartPosX = perfectPathCoords[startFrame][0];
+      const ballStartPosY = perfectPathCoords[startFrame][1];
+      const hoopStartPosX = videoMetadata.hoop_bbox[0];
+      const hoopStartPosY = videoMetadata.hoop_bbox[1];
+      console.log(perfectPathCoords)
+      const ratioX = videoElementRef.current?.width /(perfectPathCoords[startFrame][0]-perfectPathCoords[endFrame][0]);
+      const ratioY = videoElementRef.current.height/(perfectPathCoords[startFrame][1]-perfectPathCoords[endFrame][1]);
+      const context = canva.current?.getContext('2d');
+      console.log("X" + ratioX);
+      console.log("Y" + ratioY);
+      
+      //context?.drawImage(videoElementRef.current, 0, 0, canva.current?.width, canva.current.height);
+      context.fillStyle = "red";
+      for(let i = startFrame; i <= endFrame; i++){
+        const x = (perfectPathCoords[i][0] ) * (videoElementRef.current.width / 1920)  ;
+        const y = (perfectPathCoords[i][1] )* (videoElementRef.current.height/1080);
+        console.log("x" + x + "y" + y);
+       
+        context?.fillRect(x, y, 3, 3);
+      }
+     
+    }
+  },[])
   return (
     <div>
-      <video ref={videoElementRef} className='w-full md:w-4/5' controls src={`http://localhost:8000/media/${videoMetadata.video}`}></video>
-      <p>dynamic segment with</p>
-      {perfectPathCoords.map((coordinateRect: Array<number>) => (
-        videoElementRef ? drawPoint(coordinateRect, videoElementRef.current.getBoundingClientRect()) : null
-      ))}
+      <video ref={videoElementRef} style={{position:"absolute"}} height = {225} width={400}  controls src={`http://localhost:8000/media/${videoMetadata.video}`}></video>
+      <canvas ref = {canva} style ={{position:'absolute', pointerEvents:"none", left:videoElementRef.current?.getBoundingClientRect().left, top: videoElementRef.current?.getBoundingClientRect().top}}></canvas>
     </div>
   )
 }
